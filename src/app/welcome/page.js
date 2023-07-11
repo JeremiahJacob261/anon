70938aa345613fd57bb44c63e07db3fc01e86601
Jeremiah <jeremiahajacob261@gmail.com>
2023-07-11 23:46:47 +0100

    fix

--- src/app/welcome/page.js
+++ src/app/welcome/page.js
@@ -3,13 +3,18 @@ import { onAuthStateChanged } from "firebase/auth";
 import { useState,useEffect } from "react";
 import {app} from '../../api/firebase'
 import { getAuth,signOut } from "firebase/auth";
-import { Divider, Stack, Typography, Button,Box } from "@mui/material";
+import { Divider, Stack, Typography, Button,Box, Container,TextField } from "@mui/material";
 import { supabase } from "../../api/supabase";
 import { useRouter } from "next/navigation";
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 export default function Welcome() {
     const auth = getAuth(app);
     const router = useRouter();
-    const [useri,setUser] = useState('')
+    const [useri,setUser] = useState('');
+    const [title,setTitle] = useState('');
+    const [lists,setLists] = useState([]);
+      const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     useEffect(()=>{
         onAuthStateChanged(auth, (user) => {
           if (user) {
@@ -25,23 +30,83 @@ export default function Welcome() {
             router.push('/');
           }
         });
-      },[]);
+        const getL = async()=>{
+          const {data,error} = await supabase
+          .from('topics')
+          .select()
+          .eq('email',useri.email)
+          setLists(data);
+          console.log(data)
+          console.log(useri.email)
+        }
+        getL();
+      },[setLists]);
       const signOuts =()=>{
         signOut(auth).then(()=>{
           
           router.push('/');
         })
       }
+//generate random string
+const generateString = (length)=> {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+//end of random string
+//create random de topic
+const topic = async()=>{
+  const {data,error} = await supabase
+  .from('topics')
+  .insert({
+    'title':title,
+    'email':useri.email,
+    'code':generateString(5),
+  })
+  console.log(error)
+
+  setTitle('');
+}
+//end topic creation
+
+function Lister(){
+  try{
+    lists.map((l)=>{
+      console.log(l.title)
+      return(
+        <div>
+         hello, {l.title}
+        </div>
+      )
+    })
+  }catch(e){
+    console.log(e);
+  }
+}
     return(
         <Box style={{padding:'8px',height:'100vh'}}>
+          
 <Typography variant="subtitle1">Welcome, {useri.displayName}</Typography>
 <Stack direction="column">
-  <Button variant="contained">Create a Topic</Button>
+    <TextField variant='filled' 
+    style={{height:'40px',width:'200px'}}
+    value={title} onChange={(e)=>setTitle(e.target.value)}/>
+  <Button variant="contained" onClick={topic}>Create a Topic</Button>
       <Typography variant="button">Recent</Typography>
       <Divider/>
+<Stack direction='column' style={
+  {
+    height:'200px'
+  }
+}>
+  <Lister/>
+</Stack>
 <Button onClick={signOuts}>Log out</Button>
 </Stack>
-
         </Box>
     )
 }
