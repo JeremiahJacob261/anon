9b8173dc8761834e88fce3d9a09657fab9d5437e
Jeremiah <jeremiahajacob261@gmail.com>
2023-07-11 11:46:47 +0100

    c3

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
 export default function Home() {
   return (
     <main className={styles.main}>
       <div style={{display:'flex',justifyContent:'center'}}>
-      <h3>ANON</h3>
+      <h1>ANON</h1>
+      <p>Welcome to ANON, the most trusted Anonymous Messaging App</p>
+      
       </div>
     </main>
   )
